77b6863cd064cce2817bd7a4e8ea940067548462
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-08-04 11:08:27 +0200

    UserManager: Add functions subject_for_token and resource_from_claims

--- lib/xute/user_manager.ex
+++ lib/xute/user_manager.ex
@@ -8,6 +8,17 @@ defmodule Xute.UserManager do
 
   alias Xute.UserManager.User
 
+  def subject_for_token(user, _claims) do
+    {:ok, to_string(user.id)}
+  end
+
+  def resource_from_claims(%{"sub" => id}) do
+    case UserManager.get_user!(id) do
+      nil -> {:error, :resource_not_found}
+      user -> {:ok, user}
+    end
+  end
+
   @doc """
   Returns the list of users.
 
