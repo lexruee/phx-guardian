4451f748f625e1fc4fb43b9a48bac666f97746b9
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-08-04 12:10:30 +0200

    Implement function login_reply

--- lib/xute_web/controllers/session_controller.ex
+++ lib/xute_web/controllers/session_controller.ex
@@ -15,7 +15,21 @@ defmodule XuteWeb.SessionController do
 
 
   def login(conn, %{"user" => %{"username" => username, "password" => password}}) do
+    UserManager.authenticate_user(username, password)
+    |> login_reply(conn)
+  end
 
+  defp login_reply({:ok, user}, conn) do
+    conn
+    |> put_flash(:success, "Welcome back, #{user.username}!")
+    |> Guardian.Plug.sign_in(user)
+    |> redirect(to: "/secret")
+  end
+
+  defp login_reply({:error, reason}, conn) do
+    conn
+    |> put_flash(:error, to_string(reason))
+    |> new(%{})
   end
 
   def logout(conn, _) do
