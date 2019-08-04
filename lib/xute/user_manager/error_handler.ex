dfd5b815d5f95a512e981e1ea766ab5ca3bbeedb
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-08-04 11:50:46 +0200

    Fix typo

--- lib/xute/user_manager/error_handler.ex
+++ lib/xute/user_manager/error_handler.ex
@@ -7,7 +7,7 @@ defmodule Xute.UserManager.ErrorHandler do
   def auth_error(conn, {type, _reason}, _opts) do
     body = to_string(type)
     conn
-    |> put_resp_content("text/plain")
+    |> put_resp_content_type("text/plain")
     |> send_resp(401, body)
   end
 end
