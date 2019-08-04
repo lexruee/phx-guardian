5decaa2438a086bc8652b8ab3bee4638e973f61f
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-08-04 11:52:05 +0200

    Add secret page

--- lib/xute_web/controllers/page_controller.ex
+++ lib/xute_web/controllers/page_controller.ex
@@ -1,6 +1,11 @@
 defmodule XuteWeb.PageController do
   use XuteWeb, :controller
 
+  def secret(conn, _params) do
+    user = Guardian.Plug.current_resource(conn)
+    render(conn, "secret.html", current_user: user)
+  end
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
